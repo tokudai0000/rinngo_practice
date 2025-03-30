71ec24cb9e013b81e748c5928c60ab3f52e793e1
rinngo0302 <rinngoyuki03020302@gmail.com>
2025-03-30 21:24:58 +0900

    SwiftUIの初期化

---
+++ rinngo_practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rinngo_practice
+//
+//  Created by 齋藤祐希 on 2025/03/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
