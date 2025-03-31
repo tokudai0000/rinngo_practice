472594444c41362aeada0325a50bc231ed477182
rinngo0302 <rinngoyuki03020302@gmail.com>
2025-03-31 19:01:28 +0900

    3の倍数のときに赤になり、それ以外のときは黒になる機能

--- rinngo_practice/ContentView.swift
+++ rinngo_practice/ContentView.swift
@@ -9,18 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     @State var count: Int = 0
+    @State var textColor: Color = .black
     var body: some View {
         VStack {
             Text(String(count))
                 .font(.largeTitle)
                 .fontWeight(.regular)
+                .foregroundColor(textColor)
             HStack {
                 Button("Reset") {
                     count = 0
+                    
+                    textColor = .black
                 }
 
                 Button("Tap") {
                     count += 1
+                    
+                    if (count % 3 == 0) {
+                        textColor = .red
+                    } else {
+                        textColor = .black
+                    }
                 }
             }
         }
